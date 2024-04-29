4877afb0a331784152c56e66ad256e7feea0bbaa
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-04-29 19:25:40 +0200

    removing fragment

--- src/components/Story/StoryThumbail.jsx
+++ src/components/Story/StoryThumbail.jsx
@@ -1,10 +1,8 @@
 const StoryThumbnail = ({ storyObject }) => {
   return (
-    <>
       <a href={storyObject.url} target="_blank" rel="noopener noreferrer">
         <img src="https://placehold.co/120x80" width="120" height="80" />
       </a>
-    </>
   );
 };
 
