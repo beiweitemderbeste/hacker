1114d2c7b3d3924d4d75bbdd16affd7d6a1e05cd
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-04-30 20:28:50 +0200

    moving h1 into separate component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import HeaderTitle from  "./HeaderTitle"
+
 const Header = () => {
   return (
-    <header className="bg-gray-900 text-white py-4 text-center">
-      <h1 className="text-3xl font-bold">Hacker News</h1>
+    <header className="bg-gray-900 py-4 text-center">
+      <HeaderTitle text="hacker news" />
     </header>
   );
 };
---
+++ src/components/HeaderTitle.jsx
@@ -0,0 +1,5 @@
+const HeaderTitle = ({ text }) => {
+  return <h1 className="text-3xl font-bold text-white">{text}</h1>;
+};
+
+export default HeaderTitle;
