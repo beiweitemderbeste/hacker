df15f5ecf40b424571c1ca1d1ce8802f4e2c402b
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-05-01 11:09:46 +0200

    renaming variables

--- src/components/Story/StoryHeader.jsx
+++ src/components/Story/StoryHeader.jsx
@@ -1,13 +1,13 @@
 import { convertUnixTime, shortURL } from "../../utils/utils";
 
 const StoryHeader = ({ storyObject }) => {
-  const time = convertUnixTime(storyObject.time);
+  const formattedTime = convertUnixTime(storyObject.time);
 
   return (
     <div className="flex">
       <p className="text-white mr-2">{storyObject.by}</p>
       <p className="text-gray-500 mr-1"> ⚬ </p>
-      <p className="text-gray-500 mr-1">{time}</p>
+      <p className="text-gray-500 mr-1">{formattedTime}</p>
       <p className="text-gray-500 mr-1"> ⚬ </p>
       {storyObject.url && <p className="text-gray-500">{shortURL(storyObject.url)}</p>}
     </div>
