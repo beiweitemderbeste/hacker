41bd7f0111770c4e3c197f702a7bcabc2e940faf
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-05-01 11:22:31 +0200

    moving story author into separate component

---
+++ src/components/Story/StoryAuthor.jsx
@@ -0,0 +1,5 @@
+const StoryAuthor = ({ author }) => {
+  return <p className="text-white mr-2">{author}</p>;
+};
+
+export default StoryAuthor
--- src/components/Story/StoryHeader.jsx
+++ src/components/Story/StoryHeader.jsx
@@ -1,12 +1,14 @@
 import { convertUnixTime, shortURL } from "../../utils/utils";
 
+import StoryAuthor from "./StoryAuthor";
+
 const StoryHeader = ({ storyObject }) => {
   const formattedTime = convertUnixTime(storyObject.time);
-  const formattedURL = shortURL(storyObject.url)
+  const formattedURL = shortURL(storyObject.url);
 
   return (
     <div className="flex">
-      <p className="text-white mr-2">{storyObject.by}</p>
+      <StoryAuthor author={storyObject.by} />
       <p className="text-gray-500 mr-1"> ⚬ </p>
       <p className="text-gray-500 mr-1">{formattedTime}</p>
       <p className="text-gray-500 mr-1"> ⚬ </p>
