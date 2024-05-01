2bd7af0b03a8cd37adb69d675a5eaf9f7648eb9f
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-05-01 11:29:30 +0200

    moving story time into separate component

--- src/components/Story/StoryHeader.jsx
+++ src/components/Story/StoryHeader.jsx
@@ -1,17 +1,15 @@
-import { convertUnixTime, shortURL } from "../../utils/utils";
+import { shortURL } from "../../utils/utils";
 
 import StoryAuthor from "./StoryAuthor";
+import StoryTime from "./StoryTime"
 
 const StoryHeader = ({ storyObject }) => {
-  const formattedTime = convertUnixTime(storyObject.time);
   const formattedURL = shortURL(storyObject.url);
 
   return (
     <div className="flex">
       <StoryAuthor author={storyObject.by} />
-      <p className="text-gray-500 mr-1"> ⚬ </p>
-      <p className="text-gray-500 mr-1">{formattedTime}</p>
-      <p className="text-gray-500 mr-1"> ⚬ </p>
+      <StoryTime time={storyObject.time} />
       {storyObject.url && <p className="text-gray-500">{formattedURL}</p>}
     </div>
   );
---
+++ src/components/Story/StoryTime.jsx
@@ -0,0 +1,9 @@
+import { convertUnixTime } from "../../utils/utils";
+
+const StoryTime = ({ time }) => {
+  const formattedTime = convertUnixTime(time);
+
+  return <p className="text-gray-500 mr-1"> ⚬ ${formattedTime} ⚬ </p>;
+};
+
+export default StoryTime;
