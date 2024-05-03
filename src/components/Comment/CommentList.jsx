7b6e3b4586117091349a7cafd1a5cc28eff63323
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-05-03 15:56:53 +0200

    bugfixing

--- src/components/Comment/CommentList.jsx
+++ src/components/Comment/CommentList.jsx
@@ -1,11 +1,10 @@
 import Comment from "./Comment";
 
 const CommentList = ({ commentIDs }) => {
-  console.log("commentIDs: ", commentIDs)
   return (
     <div className="container mx-auto bg-gray-900">
       <ul className="list-none m-0 p-0">
-        {commentIDs.map((commentID) => (
+        {commentIDs && commentIDs.map((commentID) => (
           <Comment key={commentID} commentID={commentID} />
         ))}
       </ul>
--- src/components/Story/StoryModalBody.jsx
+++ src/components/Story/StoryModalBody.jsx
@@ -1,19 +1,11 @@
 import CommentList from "../Comment/CommentList";
-import StoryHeader from "./StoryHeader";
-import StoryBody from "./StoryBody";
-import StoryFooter from "./StoryFooter";
+import Story from "./Story";
 
 const StoryModalBody = ({ storyObject }) => {
   return (
     <div>
-      <StoryHeader
-        author={storyObject.by}
-        time={storyObject.time}
-        link={storyObject.url}
-      />
-      <StoryBody title={storyObject.title} link={storyObject.url} />
-      <StoryFooter commentCount={storyObject.kids ? storyObject.kids.length : 0} score={storyObject.score} />
-      {storyObject.kids && <CommentList commentIDs={storyObject.kids} />}
+      <Story storyObject={storyObject} />
+      <CommentList commentIDs={storyObject.kids} />
     </div>
   );
 };
